cbd545570aa61a8c2fcc73cc0dafe8a2da9d4948
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2020-10-31 22:34:01 +0100

    BFS from Grokking book

---
+++ src/main/java/guga/bfs/grokking/BreadthFirstSearch.java
@@ -0,0 +1,38 @@
+package guga.bfs.grokking;
+
+import java.util.*;
+
+public class BreadthFirstSearch {
+
+    public boolean searchBy(Map<String, List<String>> graph, String elementToFind) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean search(Map<String, List<String>> graph, String name) {
+        Queue<String> searchQueue = new ArrayDeque<>(graph.get(name));
+        // This list is how you keep track of which people you've searched before.
+        List<String> searched = new ArrayList<>();
+
+        while (!searchQueue.isEmpty()) {
+            String person = searchQueue.poll();
+            // Only search this person if you haven't already searched them
+            if (!searched.contains(person)) {
+                if (person_is_seller(person)) {
+                    System.out.println(person + " is a mango seller!");
+                    return true;
+                } else {
+                    searchQueue.addAll(graph.get(person));
+                    // Marks this person as searched
+                    searched.add(person);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean person_is_seller(String name) {
+        //Fixme
+        return name.equals("thom");
+    }
+}
